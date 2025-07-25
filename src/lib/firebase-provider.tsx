1a8e29884358697b32302bf537659e24602c1292
Fábio Setubal <fabio.setuball@gmail.com>
2025-07-25 02:04:23 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/lib/firebase-provider.tsx
+++ src/lib/firebase-provider.tsx
@@ -1,12 +1,14 @@
 "use client"
 
 import { app, auth } from './firebase';
-import { AuthProvider } from 'reactfire';
+import { AuthProvider, FirebaseAppProvider } from 'reactfire';
 
 export function FirebaseProvider({ children }: { children: React.ReactNode }) {
     return (
-        <AuthProvider sdk={auth}>
-            {children}
-        </AuthProvider>
+        <FirebaseAppProvider firebaseApp={app}>
+            <AuthProvider sdk={auth}>
+                {children}
+            </AuthProvider>
+        </FirebaseAppProvider>
     );
 }
